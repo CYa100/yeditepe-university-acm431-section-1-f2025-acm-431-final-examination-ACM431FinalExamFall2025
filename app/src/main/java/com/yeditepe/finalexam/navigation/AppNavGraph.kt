61f0621d5035e357cd334009bf7733646b1d97a6
Cemşah Y <cemyaydan001@gmail.com>
2026-01-06 14:49:31 +0300

    Update AppNavGraph.kt

--- app/src/main/java/com/yeditepe/finalexam/navigation/AppNavGraph.kt
+++ app/src/main/java/com/yeditepe/finalexam/navigation/AppNavGraph.kt
@@ -15,7 +15,20 @@ fun AppNavGraph() {
     ) {
 
         // TODO 1: Add composable for "tasks"
+        composable("tasks") {
+            TaskListScreen(navController = navController, viewModel = viewModel)
+        }
 
         // TODO 2: Add composable for "taskDetail/{title}"
+        composable(
+            "taskDetail/{title}",
+            arguments = listOf(navArgument("title") { type = NavType.IntType })
+        ) {
+            backStackEntry ->
+            val task = backStackEntry.arguments?.getInt("title") ?: -1
+            if (task >= 0) {
+                TaskDetailScreen(task = task, viewModel = viewModel)
+            }
+        }
     }
 }
